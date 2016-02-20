6aa3d0994e00bd577d070cb23763bc203b240582
Yasunobu Igarashi <yasunobu.igarashi@gmail.com>
2016-02-20 15:29:41 +0900

    なんとか進めた3

--- main.js
+++ main.js
@@ -5,6 +5,13 @@ var
     port = process.env.PORT || 3000,
     parser = require('body-parser');
 
+var knex = require('knex')({
+	client: 'sqlite3',
+	connection: {
+	    filename ":memory:"
+	}
+    });
+
 app.use(parser.json());
 app.use(express.static(__dirname + '/public'))
 
@@ -14,8 +21,18 @@ app.use(express.static(__dirname + '/public'))
 	});
 
     app.get('/api/projects', function (req, res, next) {
-	    res.json('実装します');
-	    return next();
+	    knex.select('*').from('projects')
+		.then(function(projects){
+			res.json(projects);
+			return next();
+		    });
+	    .catch(function(error){
+		    res.status(500).json(error);
+			return next();
+		});
+
+	    /**	    	    res.json('実装します');
+			    return next(); */
 	});
 
 
@@ -32,3 +49,24 @@ app.post('/api/badrequest', function (req, res, next) {
     app.listen(port, function () {
 	    console.log('Server running with port', port);
 	});
+
+
+/** @ToDo
+ * Initialize database
+ * this is for 'in-memory' database and should be removed
+ */
+var sqls = require('fs')
+    .readFileSync(__dirname + '/specifications/database.sql')
+    .toString();
+
+knex.raw(sqls)
+    .then(function () {
+	    /** @ToDo
+	     * Run server after database initialization
+	     * this is for 'in-memory' database and should be removed
+	     */
+	    app.listen(port, function () {
+		    console.log("Server running with port", port)
+			});
+	});
+
