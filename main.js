ea9441dfa4328b23904e8fee14955959c7e793ab
Yasunobu Igarashi <yasunobu.igarashi@gmail.com>
2016-02-20 15:32:39 +0900

    なんとか進めた6

--- main.js
+++ main.js
@@ -46,9 +46,9 @@ app.post('/api/badrequest', function (req, res, next) {
 	return next();
     })
 
-    app.listen(port, function () {
+/**    app.listen(port, function () {
 	    console.log('Server running with port', port);
-	});
+	    }); */
 
 
 /** @ToDo
